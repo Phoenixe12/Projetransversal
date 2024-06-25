1fed1560fcc1953e9ed1902a93641693c7c7aaf6
El Anewar <dmlattanda@gmail.com>
2024-06-25 20:02:18 +0000

    modification du main

--- resources/views/layouts/admin-layout/main.blade.php
+++ resources/views/layouts/admin-layout/main.blade.php
@@ -117,11 +117,6 @@
                 <i class="bi bi-archive"></i><span>Entreprise</span>
             </a>
         </li><!-- End Facture Nav -->
-        <li>
-            <a class="nav-link collapsed" href="{{ route('Gestion+Country.index') }}">
-                <i class="bi bi-flag"></i><span>Pays</span>
-            </a>
-        </li>
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('Gestion+Profil.index') }}">
@@ -129,6 +124,12 @@
             </a>
 
         </li><!-- End Messagerie Nav -->
+        <li>
+            <a class="nav-link collapsed" href="{{ route('Gestion+Country.index') }}">
+                <i class="bi bi-flag"></i><span>Pays</span>
+            </a>
+        </li>
+
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('Gestion+Compte.index') }}">
                 <i class="bi bi-building-add"></i><span>Compte</span>
